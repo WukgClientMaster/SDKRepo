653be62938c22b701d0ee844ea61613e5e9eaf8b
吴可高 <wukegao@wukegaodeMacBook-Pro.local>
2019-12-23 14:36:42 +0800

    删除不应该存在的玩意儿

--- Example/Pods/Target Support Files/Pods-SDKRepo_Tests/Pods-SDKRepo_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SDKRepo_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SDKRepo_Tests
-@end
--- Example/Pods/Target Support Files/SDKRepo/SDKRepo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SDKRepo : NSObject
-@end
-@implementation PodsDummy_SDKRepo
-@end
